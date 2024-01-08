c57fad92906404d92b91f7188ebcd1aac852a919
thesiddharth24 <siddharth20it028@gmail.com>
2024-01-08 17:54:17 +0530

    done

--- models/tourModel.js
+++ models/tourModel.js
@@ -163,10 +163,10 @@ tourSchema.pre(/^find/, function (next) {
   next();
 });
 
-tourSchema.post(/^find/, function (docs, next) {
-  console.log(`Query took ${Date.now() - this.start}ms`);
-  next();
-});
+// tourSchema.post(/^find/, function (docs, next) {
+//   console.log(`Query took ${Date.now() - this.start}ms`);
+//   next();
+// });
 
 //Aggregation Middleware
 // tourSchema.pre('aggregate', function (next) {
